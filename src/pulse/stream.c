7c3fd2f8493dda6305ed5ccdfce0fa666b858495
Jonathan Marler <johnnymarler@gmail.com>
2025-08-07 17:15:57 -0600

    fix array out-of-bounds ubsan in stream_get_timing_info_callback
    This issue was found by enabling ubsan. For me it consistently triggered
    after about 28 seconds running a simple example that plays a sine wave
    via the mainloop api.
    
    I added a log and confirmed that before the ubsan is triggered the
    index variable j is indeed 32 which is out-of-bounds.

--- src/pulse/stream.c
+++ src/pulse/stream.c
@@ -1915,7 +1915,7 @@ static void stream_get_timing_info_callback(pa_pdispatch *pd, uint32_t command, 
 
             /* Go through the saved correction values and add up the
              * total correction.*/
-            for (n = 0, j = o->stream->current_write_index_correction+1;
+            for (n = 0, j = (o->stream->current_write_index_correction+1) % PA_MAX_WRITE_INDEX_CORRECTIONS;
                  n < PA_MAX_WRITE_INDEX_CORRECTIONS;
                  n++, j = (j + 1) % PA_MAX_WRITE_INDEX_CORRECTIONS) {
 
